f42df522040e6a9e3bf1ddbb668b70cc76b10fe1
Justin Waltz <justinwaltz@Justins-iMac.local>
2019-09-06 16:34:27 -0500

    basic App and Header components rendering with css

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,11 +1,38 @@
+//Dependancies
 import React from 'react'
+//Imported Components
+import Header from './Header.js'
+import SideBar from './SideBar.js'
 
+//React Class
 class App extends React.Component {
-  render () {
-    return (
-      <h1>Rails and React 4Eva!</h1>
-    )
-  }
+	//State
+	constructor(props) {
+		super(props)
+		this.state = {
+			loggedInUser: "User",
+			view: {
+				page: 'home',
+				pageTitle: 'Welcome to listful'
+			}
+		}
+	}
+	//Methods
+	handleView = (view, user) => {
+		let pageTitle = '';
+		switch (view) {
+			case 'home':
+				pageTitle = `Hi ${user}, Welcome back`;
+			break
+		}
+	}
+	render () {
+    	return (
+      		<div className="container">
+				<Header loggedInUser={this.state.loggedInUser}/>
+			</div>
+    	)
+  	}
 }
 
 export default App
---
+++ app/javascript/components/SideBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class SideBar extends React.Component {
+
+	render () {
+		return (
+			<div>
+				<ul>
+					<li>the classic to-do</li>
+					<li>movies</li>
+					<li>music</li>
+					<li>games</li>
+					<li>books</li>
+					<li>videos</li>
+					<li>bucket</li>
+				</ul>
+			</div>
+		)
+	}
+}
