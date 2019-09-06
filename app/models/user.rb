0e33588e7ba7581cc6092185e6aa3f8d96efdc92
Justin Waltz <justinwaltz@Justins-iMac.local>
2019-09-06 11:40:14 -0500

    postgres

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 
 class User
-	DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'listful'})
+	if(ENV['DATABASE_URL'])
+        uri = URI.parse(ENV['DATABASE_URL'])
+        DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+    else
+        DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'listful'})
+    end
 	def self.all
 		results = DB.exec("SELECT * FROM users;")
 		results.each do |result|
