9d2318896966002feb2a9ef3d03637996d31d920
Justin Waltz <justinwaltz@Justins-iMac.local>
2019-09-07 15:47:14 -0500

    working on submit form

--- app/javascript/components/Main.js
+++ app/javascript/components/Main.js
@@ -4,17 +4,35 @@ import Movies from './Movies.js'
 
 
 class Main extends React.Component {
-	// constructor(props) {
-	// 	super(props)
-	// 	this.state = {
-	//
-	// 	}
-	// }
+
+	handleCreate = (createData) => {
+	    fetch('/api/posts', {
+	      body: JSON.stringify(createData),
+	      method: 'Post',
+	      headers: {
+	        'Accept': 'application/json, text/plain, */*',
+	        'Content-Type': 'application/json'
+	      }
+	    })
+	      .then(createdPost => {
+	        return createdPost.json()
+	      })
+	      .then(jsonedPost => {
+	        // takes us back to the index page
+	        this.props.handleView('home')
+	        // update state with our new post
+	        this.setState(prevState => {
+	          prevState.posts.push(jsonedPost)
+	          return { posts: prevState.posts }
+	        })
+	      })
+	      .catch(err => console.log(err))
+	  }
 
 	render () {
 		if (this.props.listToShow === 'Todos') {
 			return (
-				<Todos listItemsArray={this.props.listItemsArray}/>
+				<Todos handleCreate={this.handleCreate} listItemsArray={this.props.listItemsArray}/>
 			)
 		} else if (this.props.listToShow === 'Movies') {
 			return (
--- app/javascript/components/Todos.js
+++ app/javascript/components/Todos.js
@@ -1,24 +1,56 @@
 import React from 'react'
 
 class Todos extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			addForm: false,
+			view: 'list',
+			formData: {
+				
+			}
+		}
+	}
+	toggleAddForm = () => {
+		this.setState({ addForm: !this.state.addForm })
+	}
+	handleSubmit = (event) => {
+		event.preventDefault()
+		this.props.handleCreate(this.state.formData)
+	}
 
 	render () {
-		return (
-			<div>
-				{
-					this.props.listItemsArray.map((itemData) => (
-						<div className="todo-div" key={itemData.id}>
-						{itemData.iscomplete ? (
-							<input type="checkbox" defaultChecked />
-						) : (
-							<input type="checkbox"/>
-						)}
+		if (this.state.addForm) {
+			return (
+				<main>
+					<form onSubmit={this.handleSubmit}>
+						<label htmlFor="description">New To Do Item</label> <br/>
+						<input type="text" id="description"/>
+						<h2 type="submit" onClick={this.AddItem} className="list-add-btn"><i className="material-icons md-36">add</i></h2>
+					</form>
+				</main>
+			)
+		} else {
+			return (
+				<main>
+					<h2 onClick={this.toggleAddForm} className="list-add-btn"><i className="material-icons md-36">add</i></h2>
+					{
+						this.props.listItemsArray.map((itemData) => (
+							<div className="todo-div" key={itemData.id}>
+							{itemData.iscomplete ? (
+								<input type="checkbox" defaultChecked />
+							) : (
+								<input type="checkbox"/>
+							)}
 							<h3>{itemData.description}</h3>
-						</div>
-					))
-				}
-			</div>
-		)
+							</div>
+						))
+					}
+				</main>
+			)
+		}
 	}
 }
+
+
 export default Todos
