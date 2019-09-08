be044876757cea8c6df9b4d430cb3b6b4fa245b1
Justin Waltz <justinwaltz@Justins-iMac.local>
2019-09-08 11:15:29 -0500

    add form working w/o database

--- app/javascript/components/Todos.js
+++ app/javascript/components/Todos.js
@@ -7,33 +7,48 @@ class Todos extends React.Component {
 			addForm: false,
 			view: 'list',
 			formData: {
-				
+				description: ''
 			}
 		}
+		this.handleChange = this.handleChange.bind(this)
 	}
 	toggleAddForm = () => {
 		this.setState({ addForm: !this.state.addForm })
 	}
+
+	handleChange = (event) => {
+		this.setState({
+			formData: {
+				[event.target.id] : event.target.value
+			}
+		})
+	}
+
 	handleSubmit = (event) => {
 		event.preventDefault()
-		this.props.handleCreate(this.state.formData)
+		if(this.state.addform) {
+      		this.props.handleCreate(this.state.formData, 'Todo')
+    	}
 	}
 
 	render () {
 		if (this.state.addForm) {
 			return (
 				<main>
+					<h2><i onClick={this.toggleAddForm} className="material-icons md-36">close</i></h2>
 					<form onSubmit={this.handleSubmit}>
-						<label htmlFor="description">New To Do Item</label> <br/>
-						<input type="text" id="description"/>
-						<h2 type="submit" onClick={this.AddItem} className="list-add-btn"><i className="material-icons md-36">add</i></h2>
+						<label htmlFor="description">New To Do Item</label>
+							<input type="text" id="description"
+								value={this.state.formData.description}
+								onChange={this.handleChange}/>
+						<button type="submit" onClick={this.AddItem} className="list-add-btn"><i className="material-icons md-36">add</i></button>
 					</form>
 				</main>
 			)
 		} else {
 			return (
 				<main>
-					<h2 onClick={this.toggleAddForm} className="list-add-btn"><i className="material-icons md-36">add</i></h2>
+					<button onClick={this.toggleAddForm} className="list-add-btn"><i className="material-icons md-36">add</i></button>
 					{
 						this.props.listItemsArray.map((itemData) => (
 							<div className="todo-div" key={itemData.id}>
