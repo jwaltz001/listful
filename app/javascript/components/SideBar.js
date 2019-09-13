6293ad7f2a495b94bc5ecc6ddbe52d1041a5e1a0
Justin Waltz <justinwaltz@Justins-iMac.local>
2019-09-13 12:15:32 -0500

    fixed checkbox functionality on todo list, made temp screen for not functioning lists

--- app/javascript/components/SideBar.js
+++ app/javascript/components/SideBar.js
@@ -5,16 +5,17 @@ class SideBar extends React.Component {
 	render () {
 			return (
 				<div className="sidebar">
+					<h2 className="sidebar-title">your lists</h2>
 					<ul>
 						<li onClick={() => {this.props.fetchItems('Todos')}}>the classic to-do</li>
 						<li onClick={() => {this.props.fetchItems('Movies')}}>movies</li>
-						<li>videos</li>
-						<li>games</li>
-						<li>books</li>
-						<li>bucket</li>
+						<li onClick={() => {this.props.fetchItems('TempScreen')}}>videos</li>
+						<li onClick={() => {this.props.fetchItems('TempScreen')}}>games</li>
+						<li onClick={() => {this.props.fetchItems('TempScreen')}}>books</li>
+						<li onClick={() => {this.props.fetchItems('TempScreen')}}>bucket</li>
 						<br/>
 						<br/>
-						<li><i onClick={this.props.handleToggleClick} className="material-icons md-36">add</i> Create New </li>
+						<li onClick={() => {this.props.fetchItems('TempScreen')}}><i className="material-icons md-36">add</i> Create New </li>
 					</ul>
 				</div>
 			)
